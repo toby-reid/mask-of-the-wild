6efc06da3968f426774ce8fc11da5cde7a90ff21
Toby Reid <toby.reid@usu.edu>
2026-01-30 19:17:58 -0700

    Started scaffolding

---
+++ Global/Global.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Global
+{
+    public class ControlBindings
+    {
+        // Disallow instantiation
+        private ControlBindings() {}
+
+        public const string MoveUp = "move_up";
+        public const string MoveDown = "move_down";
+        public const string MoveLeft = "move_left";
+        public const string MoveRight = "move_right";
+
+        public const string ButtonA = "action_a";
+        public const string ButtonB = "action_b";
+
+        public const string Start = "start";
+        public const string Select = "select";
+    }
+}
