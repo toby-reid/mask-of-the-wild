d79368d53da20234b7524fd37dcafb682a20ea7f
Origamiboss <vincentclimer@gmail.com>
2026-01-30 22:25:12 -0600

    I played around a bit more

--- Scenes/Overworld/Placeholders/PlaceHolderCursorEventListener.cs
+++ Scenes/Overworld/Placeholders/PlaceHolderCursorEventListener.cs
@@ -3,14 +3,38 @@ using System;
 
 public partial class PlaceHolderCursorEventListener : Node2D
 {
-    [Export] public CursorListener cursorListener;
+    [Export] public CursorListener[] cursorListeners;
+    [Export] public string[] cursorStrings; // Make sure this array length matches cursorListeners
+    [Export] public DialoguePlaceholder dialoguePlaceholder;
 
     public override void _Ready()
     {
-        cursorListener.OnClicked += HandleCursorClicked;
+        // Subscribe to OnClicked for all listeners
+        for (int i = 0; i < cursorListeners.Length; i++)
+        {
+            if (cursorListeners[i] != null)
+                cursorListeners[i].OnClicked += HandleCursorClicked;
+        }
+
+        // Optional sanity check
+        if (cursorListeners.Length != cursorStrings.Length)
+            GD.PrintErr("cursorListeners and cursorStrings arrays are not the same length!");
     }
+
     private void HandleCursorClicked(object? sender, EventArgs e)
     {
-        GD.Print("CursorListener reported a click event!");
+        if (sender is not CursorListener clickedListener)
+            return;
+
+        // Find which listener was clicked
+        for (int i = 0; i < cursorListeners.Length; i++)
+        {
+            if (cursorListeners[i] == clickedListener)
+            {
+                // Set the corresponding dialogue
+                dialoguePlaceholder.SetDialogue(cursorStrings[i]);
+                return;
+            }
+        }
     }
 }
