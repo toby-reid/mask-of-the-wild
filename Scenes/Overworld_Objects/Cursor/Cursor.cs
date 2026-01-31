e6c594517e9f1e176462cdddfc87b0274cbb794a
Toby Reid <itsToby12@gmail.com>
2026-01-31 16:31:40 -0700

    Fixed cursor clamping

--- Scenes/Overworld_Objects/Cursor/Cursor.cs
+++ Scenes/Overworld_Objects/Cursor/Cursor.cs
@@ -5,7 +5,7 @@ public partial class Cursor : Node2D
 {
 	[Export] public float Speed = 500f; // pixels per second
 	[Export] public Vector2 MinBounds = new Vector2(0, 0);
-	[Export] public Vector2 MaxBounds = new Vector2(1920, 1080);
+	[Export] public Vector2 MaxBounds = new Vector2(960, 720);
 
 	[Export] public Sprite2D cursor;
 	public override void _Process(double delta)
@@ -16,21 +16,16 @@ public partial class Cursor : Node2D
 		if (GameState.IsDialogueActive)
 			return;
 
-		if (Input.IsActionPressed(Global.Controls.MoveUp)) input.Y -= 1;
-		if (Input.IsActionPressed(Global.Controls.MoveDown)) input.Y += 1;
-		if (Input.IsActionPressed(Global.Controls.MoveLeft)) input.X -= 1;
-		if (Input.IsActionPressed(Global.Controls.MoveRight)) input.X += 1;
+		if (Input.IsActionPressed(Global.Controls.MoveUp) && GlobalPosition.Y > MinBounds.Y) input.Y -= 1;
+		if (Input.IsActionPressed(Global.Controls.MoveDown) && GlobalPosition.Y < MaxBounds.Y) input.Y += 1;
+		if (Input.IsActionPressed(Global.Controls.MoveLeft) && GlobalPosition.X > MinBounds.X) input.X -= 1;
+		if (Input.IsActionPressed(Global.Controls.MoveRight) && GlobalPosition.X < MaxBounds.X) input.X += 1;
 
 		if (input != Vector2.Zero)
 		{
 			input = input.Normalized();
 			GlobalPosition += input * Speed * (float)delta;
 		}
-
-		GlobalPosition = new Vector2(
-			Mathf.Clamp(GlobalPosition.X, MinBounds.X, MaxBounds.X),
-			Mathf.Clamp(GlobalPosition.Y, MinBounds.Y, MaxBounds.Y)
-		);
 	}
 
 	//This is to change the cursor sprite based on context
